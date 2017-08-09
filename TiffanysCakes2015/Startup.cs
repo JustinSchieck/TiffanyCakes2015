4d37b2f0977360cbe14e7eaa3852e4c6dec7453b
Justin Schieck <schieck91@gmail.com>
2017-08-09 19:00:57 -0400

    wwjd

---
+++ TiffanysCakes2015/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TiffanysCakes2015.Startup))]
+namespace TiffanysCakes2015
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
